faeede0f3223e9518e4a518e8f5b9d7bfe3ab8a7
Marcus <marcus.perry2017@gmail.com>
2024-06-17 23:22:50 -0400

    Fixing

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "default_secret_key";
-const PORT = +process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 function getDatabaseUri() {
